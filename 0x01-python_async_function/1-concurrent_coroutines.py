481820d971384ab3fa628d45a4887815c3bca93b
Reen25 <mumodoreen25@gmail.com>
2023-10-10 13:44:06 +0000

    concurrent

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,14 +2,16 @@
 '''Task 1's module.
 '''
 import asyncio
-from typing import list
+from typing import List
 
-wait_random=__import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Executes wait_random n times.
     '''
     wait_times = await asyncio.gather(
-            *tuple(map(lambda_:wait_random(max_delay),range(n)))
-            )
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
     return sorted(wait_times)
